85f70a478bfc1b94c9766cbdd7c905ea31bae0b8
Mudita Shakya <mudita123ms@gmail.com>
2026-02-09 01:13:29 +0530

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -1,37 +1,70 @@
+// import express from 'express';
+// import Connection from './database/db.js';
+// import DefaultData from './default.js';
+// import Route from './routes/route.js';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import path from 'path';
+
+
+// const  app = express();
+// const __dirname = path.resolve();
+// dotenv.config();
+
+// app.use(express.json());  
+// app.use(cors());
+// app.use('/', Route);
+
+// const PORT = process.env.PORT || 8000;
+
+
+// const username=process.env.DB_USERNAME;
+// const password=process.env.DB_PASSWORD;
+
+// Connection(username,password);
+
+// // app.use(express.static(path.join(__dirname, '/client/build')));
+
+// // app.use((req, res) => {
+// //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// // });
+// app.get('/', (req, res) => {
+//     res.send('Server is running');
+// });
+
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// DefaultData();
+
 import express from 'express';
 import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Route from './routes/route.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
 
-
-const  app = express();
-const __dirname = path.resolve();
+const app = express();
 dotenv.config();
 
-app.use(express.json());  
+app.use(express.json());
 app.use(cors());
+
 app.use('/', Route);
 
-const PORT=8000;
+const PORT = process.env.PORT || 8000;
 
-const username=process.env.DB_USERNAME;
-const password=process.env.DB_PASSWORD;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
-Connection(username,password);
+Connection(username, password);
 
-// app.use(express.static(path.join(__dirname, '/client/build')));
-
-// app.use((req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
